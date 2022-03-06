5e2c58422d8c3d689ce4ab7a7421b23a62608606
saddam-satria <ardhisaddam@gmail.com>
2022-03-06 21:10:22 +0700

    initial commit

---
+++ march/week-2/meeting-1/event.js
@@ -0,0 +1,25 @@
+// const main = () => {
+//   const root = document.getElementById('root');
+//   addButtonToRoot(root);
+// };
+
+// const addButtonToRoot = (root) => {
+//   const btn = document.createElement('button');
+
+//   btn.textContent = 'Klik';
+//   btn.style.padding = '10px';
+//   btn.style.backgroundColor = 'red';
+
+//   btn.addEventListener('click', (_e) => {
+//     btn.style.backgroundColor = 'blue';
+//   });
+
+//   root.append(btn);
+// };
+
+// main();
+
+const myFunction = () => {
+  // Do something (ketika tombol di klik)
+  console.log('berhasil di klik');
+};
